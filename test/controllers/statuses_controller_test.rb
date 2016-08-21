04896c78f07a057ad2a384a6198c78e14f3a5079
knopfler81 <knopfler42@yahoo.fr>
2016-08-21 22:25:09 +0200

    Add status validation and test

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -18,13 +18,21 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
-  test " should render the new page when logged in" do
+  test "should render the new page when logged in" do
     sign_in users(:jason)
     get :new
     assert_response :success
   end
 
-  test "should create status" do
+  test "should be logged in to post status" do
+    post :create, status: { content: "Hello"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:jason)
+
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -37,12 +45,20 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit when logged in" do
+    sign_in users(:jason)
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should redirect status update when not logged in" do
+    patch :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update status when logged in" do
+    sign_in users(:jason)
     patch :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
