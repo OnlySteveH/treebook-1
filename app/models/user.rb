ed957d3c0000bb562088d297e4004598281f5900
knopfler81 <knopfler42@yahoo.fr>
2016-08-22 15:42:51 +0200

    Added carrierwave to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  mount_uploader :avatar, AvatarUploader
+  
+  validates_presence_of   :avatar
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
+
 
   has_many :statuses
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819161109) do
+ActiveRecord::Schema.define(version: 20160822121119) do
 
   create_table "statuses", force: :cascade do |t|
     t.text     "content"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160819161109) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
